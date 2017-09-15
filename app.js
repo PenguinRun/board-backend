1f8f46f4585ee0cbe5d8c38fc7c0cb0324ba2f16
penguinrun <shanban820@gmail.com>
2017-09-15 15:50:16 +0800

    fix the api url on app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/speech', speech);
+app.use('/api/speech', speech);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
